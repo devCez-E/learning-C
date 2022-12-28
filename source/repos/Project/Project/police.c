2ed36e42fa62dead37d1e37650c48040b151a057
leesungeon <devcez82@gmail.com>
2022-12-29 00:01:03 +0900

    변수,상수,입출력

---
+++ source/repos/Project/Project/police.c
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>
+
+int police(void) {
+
+	char name[256];	
+	int age;
+	float weight;
+	float height;
+	char crimeName[256];
+
+	scanf_s("이름이 뭐에요? %s", name, sizeof(name));
+	scanf_s("나이가 어떻게 되요? %d\n", &age);
+	scanf_s("몸무게가 몇이에요? %f\n", &weight);
+	scanf_s("키가 어떻게 되요? %f\n", &height);
+	scanf_s("범죄명은? %s\n", crimeName, sizeof(crimeName));
+
+	printf("--------범죄자 조서--------\n");
+	printf("이름 :		%s", name);
+	printf("나이 :		%d", age);
+	printf("몸무게 :	%.2f", weight);
+	printf("키 :		%.2f", height);
+	printf("범죄명 :	%s", crimeName);
+
+	return 0;
+}
---
+++ source/repos/Project/Project/printfscanf.c
@@ -0,0 +1,50 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	// 정수형 변수에 대한 예제
+	int age = 12;
+	printf("%d\n", age);
+	age = 13;
+	printf("%d\n", age);
+
+	// 실수형 변수에 대한 예제
+	float f = 46.5f;
+	printf("%f\n", f);
+	printf("%.2f\n", f);
+
+	double d = 4.428;
+	printf("%lf\n", d);
+	printf("%.2lf\n", d);
+
+	// 상수형 변수에 대한 예제
+	const int YEAR = 1995;
+	printf("태어난 년도 : %d\n", YEAR);
+	//YEAR = 1996;
+	
+	// printf에 대한 예제
+	int add = 3 + 7;
+	printf("3 + 7 = %d\n", add);
+	printf("%d + %d = %d\n", 3, 7, add);
+	
+	// scanf에 대한 예제
+	int input;
+	printf("값을 입력하세요 : ");
+	scanf_s("%d", &input);
+	printf("입력값 : %d\n", input);
+
+	int one, two, three;
+	printf("3개의 정수를 입력하세요 : ");
+	scanf_s("%d %d %d", &one, &two, &three);
+	printf("첫번째 값 : %d\n", one);
+	printf("두번째 값 : %d\n", two);
+	printf("세번째 값 : %d\n", three);
+
+	// 문자(한 글자), 문자열(한 글자 이상의 여러 글자)
+	char c = 'A';
+	printf("%c\n", c);
+
+	char str[256] = "Hello World";
+	printf("%s\n", str);
+
+	return 0;
+}
